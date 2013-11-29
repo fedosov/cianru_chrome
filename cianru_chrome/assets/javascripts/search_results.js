f08195520af6d2d2c601330be59a5a8dc5a93949
Mikhail Fedosov <tbs.micle@gmail.com>
2013-11-29 15:25:33 +0400

    Адаптация к новой верстке cian

--- cianru_chrome/assets/javascripts/search_results.js
+++ cianru_chrome/assets/javascripts/search_results.js
@@ -1,24 +1,28 @@
 jQuery(function($)
 {
-	$("a[href^='http://www.cian.ru/rent/flat/']").each(function()
-	{
-		var self = $(this);
-		$.ajax(
-		{
-			"url": self.attr("href")
-		})
-		.done(function(data)
-		{
-			var row = $("<tr>");
-			var col = $("<td colspan='10'>");
-			var photos = $(data).find(".object_descr_images_w").clone();
-			photos.find("img").each(function()
-			{
-				$(this).attr("height", "200");
-			});
-			col.append(photos);
-			row.append(col);
-			self.closest("tr").after(row);
-		});
-	});
+    $("a[href^='/rent/flat/']").each(function()
+    {
+        var self = $(this),
+            originalLinkText = self.text()
+        ;
+        self.text("Загрузка...");
+        $.ajax(
+        {
+            "url": self.attr("href")
+        })
+        .done(function(data)
+        {
+            var row = $("<tr>");
+            var col = $("<td colspan='10'>");
+            var photos = $(data).find(".object_descr_images_w").clone();
+            photos.find("img").each(function()
+            {
+                $(this).attr("height", "200");
+            });
+            col.append(photos);
+            row.append(col);
+            self.closest("tr").after(row);
+            self.text(originalLinkText);
+        });
+    });
 });
