1a5d889376c6932ba7e5a3a9a065c7ab6695be85
Mikhail Fedosov <tbs.micle@gmail.com>
2015-10-11 16:51:30 +0300

    Поправить для новой верстки ЦИАН

--- cianru_chrome/assets/javascripts/search_results.js
+++ cianru_chrome/assets/javascripts/search_results.js
@@ -1,33 +1,16 @@
 jQuery(function($)
 {
-    $("a[href^='/rent/flat/']").each(function()
+    $(".offer_container").each(function(k, v)
     {
-        var self = $(this),
-            originalLinkText = self.text()
-        ;
-        self.text("Загрузка...");
-        $.ajax(
+        var $container = $(v);
+        var $user_data = $container.find(".object_user_data");
+
+        $container.find(".objects_item_images_w a").each(function()
         {
-            "url": self.attr("href")
-        })
-        .done(function(data)
-        {
-            var row = $("<tr>"),
-                col = $("<td colspan='10'>"),
-                photos = $(data).find(".object_descr_images_w").clone()
-            ;
-            photos.find("img").each(function()
-            {
-                $(this).attr("height", "200");
-            });
-            photos.find("a").each(function()
-            {
-                $(this).attr("target", "_blank");
-            });
-            col.append(photos);
-            row.append(col);
-            self.closest("tr").after(row);
-            self.text(originalLinkText);
+            var self = $(this);
+            var $a = $("<a class='fancybox' target='_blank'></a>").attr("rel", "group" + k).attr("href", self.attr("href"));
+            $a.append($("<img height='150px' style='margin: 0 4px 0 0;' />").attr("src", self.attr("href")));
+            $user_data.append($a);
         });
     });
 });
